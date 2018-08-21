5554db552233da6bfdb3eb2151e0a92fe8261e4b
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-08-21 11:41:14 -0500

    Add Slot, Add Glow to Slot, Add Spin Animation to Glowing Slot

--- Project11/GameScene.swift
+++ Project11/GameScene.swift
@@ -18,8 +18,53 @@ class GameScene: SKScene {
         background.zPosition = -1
         addChild(background)
         
-        //enclose the frame in a physics body boundary, so that when the edge of the frame is reached, the box stops and bounces semi-realistically.
-        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        //placing our slots
+        makeSlot(at: CGPoint(x: 128, y: 0), isGood: true)
+        makeSlot(at: CGPoint(x: 384, y: 0), isGood: false)
+        makeSlot(at: CGPoint(x: 640, y: 0), isGood: true)
+        makeSlot(at: CGPoint(x: 896, y: 0), isGood: false)
+        
+        //place the bouncer objects in the frame
+        makeBouncer(at: CGPoint(x: 0, y: 0))
+        makeBouncer(at: CGPoint(x: 256, y: 0))
+        makeBouncer(at: CGPoint(x: 512, y: 0))
+        makeBouncer(at: CGPoint(x: 768, y: 0))
+        makeBouncer(at: CGPoint(x: 1024, y: 0))
+    }
+    
+    //creating the bouncers and placing them according to the passed param
+    func makeBouncer(at position: CGPoint) {
+        
+        let bouncer = SKSpriteNode(imageNamed: "bouncer")
+        bouncer.position = position
+        bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2)
+        bouncer.physicsBody?.isDynamic = false
+        addChild(bouncer)
+    }
+    
+    //making our slots for the user to target; slots are either good (green) or bad (red).
+    func makeSlot(at position: CGPoint, isGood: Bool) {
+        var slotBase: SKSpriteNode
+        var slotGlow: SKSpriteNode
+        
+        if isGood {
+            slotBase = SKSpriteNode(imageNamed: "slotBaseGood")
+            slotGlow = SKSpriteNode(imageNamed: "slotGlowGood")
+        } else {
+            slotBase = SKSpriteNode(imageNamed: "slotBaseBad")
+            slotGlow = SKSpriteNode(imageNamed: "slotGlowBad")
+        }
+        
+        slotBase.position = position
+        slotGlow.position = position
+        
+        addChild(slotBase)
+        addChild(slotGlow)
+        
+        //MARK: - using SKActions to make our glow rotate
+        let spin = SKAction.rotate(byAngle: .pi, duration: 10)
+        let spinForever = SKAction.repeatForever(spin)
+        slotGlow.run(spinForever)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -27,12 +72,18 @@ class GameScene: SKScene {
         //just to test things out, let's place a box where we tap
         if let touch = touches.first {
             let location = touch.location(in: self)
-            let box = SKSpriteNode(color: UIColor.red, size: CGSize(width: 64, height: 64))
             
-            //give the box a physics body of the same size
-            box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
-            box.position = location
-            addChild(box)
+            //creating a ball and giving it physics
+            let ball = SKSpriteNode(imageNamed: "ballRed")
+            
+            //using circleOfRadius to give our ball circular physics
+            ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.0)
+            
+            //restitution = bounciness. Values range from 0-1.
+            ball.physicsBody?.restitution = 0.4
+            ball.position = location
+            addChild(ball)
+
         }
     }
 }
